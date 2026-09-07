15fe9766da8d7d08b5885959886e95a5f04baf05
Nanyonga-Rahmah <nanyongarahmah891@gmail.com>
2026-09-07 16:09:52 +0300

    feat: initialise flask app

--- src/Todolist/Backend/models/todo.py
+++ src/Todolist/Backend/models/todo.py
@@ -4,8 +4,9 @@
 class Todo:
     """A class representing a todo item."""
 
-    def __init__(self, title: str, description: str, completed: bool = False):
+    def __init__(self, title: str, id: int, description: str, completed: bool = False):
         """Initialize a new todo item."""
         self.title = title
+        self.id = id
         self.description = description
         self.completed = completed
