4c1fdd9b600d5c9b5daee5cae8cc90a5d3d739df
Nanyonga-Rahmah <nanyongarahmah891@gmail.com>
2026-09-08 11:52:22 +0300

    feat: add todo controller to handle business logic

--- src/Todolist/Backend/controllers/todo.py
+++ src/Todolist/Backend/controllers/todo.py
@@ -1,1 +1,13 @@
 """Todo controller."""
+
+from Todolist.Backend.models.database_model import db
+from Todolist.Backend.models.todo import Todo
+
+
+class TodoController:
+    """A class to connect to the database."""
+
+    def get_todos(self) -> list:
+        """Get all todos from the database."""
+        todos = db.session.execute(db.select(Todo)).scalars().all()
+        return todos
