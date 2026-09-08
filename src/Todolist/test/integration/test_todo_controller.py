dd8be79007487fcf193d7df002cf5bc23c8e99b2
Nanyonga-Rahmah <nanyongarahmah891@gmail.com>
2026-09-08 11:58:19 +0300

    feat: update the controller to add todos

--- src/Todolist/test/integration/test_todo_controller.py
+++ src/Todolist/test/integration/test_todo_controller.py
@@ -9,3 +9,14 @@ def test_todo_controller_get_todos(setup) -> None:
         controller = TodoController()
         todos = controller.get_todos()
         assert isinstance(todos, list)
+
+
+def test_todo_controller_adds_a_todo(setup, todo_data) -> None:
+    """Test the add_todo function."""
+    with setup.app_context():
+        controller = TodoController()
+        todo = controller.add_todo(todo_data)
+        assert todo.id == todo_data["id"]
+        assert todo.title == todo_data["title"]
+        assert todo.description == todo_data["description"]
+        assert todo.completed == todo_data["completed"]
