73c19a5f0795dfcd9754d5fb16dc192ba039fb8e
Nanyonga-Rahmah <nanyongarahmah891@gmail.com>
2026-09-09 14:52:16 +0300

    feat: add route to handle updating of a todo

--- src/Todolist/test/test_todo_routes.py
+++ src/Todolist/test/test_todo_routes.py
@@ -1,6 +1,11 @@
 """Test Todo Routes."""
 
+from faker import Faker
 from flask.testing import FlaskClient
+
+from Todolist.Backend.models.todo import Todo
+
+fake = Faker()
 
 
 def test_todo_retrieval(test_app) -> None:
@@ -21,3 +26,30 @@ def test_todo_creation(test_app: FlaskClient, todo_data) -> None:
     response = test_app.post("/create-todo", data=(todo_data))
     assert response.status_code == 200
     assert b"title" in response.data
+
+
+def test_updating_a_todo(test_app: FlaskClient, todo_data: Todo) -> None:
+    """Test route for updating a todo."""
+    response = test_app.patch("/update-todo/1", data=(todo_data))
+    assert response.status_code == 200
+    assert b"Todo updated" in response.data
+
+
+def test_updating_todo_allows_patch_requests(test_app: FlaskClient, todo_data) -> None:
+    """Test that the update route allows patch requests."""
+    response = test_app.post("/update-todo/1", data=(todo_data))
+    assert response.status_code == 405
+
+
+def test_update_route_receives_a_todo_id(
+    test_app: FlaskClient, todo_data: Todo
+) -> None:
+    """Test the update route expects a todo_id."""
+    response = test_app.patch("/update-todo/1", data=todo_data)
+    assert response.status_code == 200
+
+
+def test_update_route_missing_a_todo_id(test_app: FlaskClient, todo_data: Todo) -> None:
+    """Test that the update route expects todo-id."""
+    response = test_app.patch("/update-todo", data=todo_data)
+    assert response.status_code == 404
